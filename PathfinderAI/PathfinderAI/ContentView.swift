c7759834e0422e4ee100c8879bb7f49d40c24949
xavier.irving <xavier.irving@hgp.hiddengeniusproject.com>
2026-09-22 20:55:12 -0700

    Create initial SwiftUI app

---
+++ PathfinderAI/PathfinderAI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PathfinderAI
+//
+//  Created by LA6_mac22 on 9/22/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PathfinderAI/PathfinderAI/PathfinderAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PathfinderAIApp.swift
+//  PathfinderAI
+//
+//  Created by LA6_mac22 on 9/22/26.
+//
+
+import SwiftUI
+
+@main
+struct PathfinderAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
